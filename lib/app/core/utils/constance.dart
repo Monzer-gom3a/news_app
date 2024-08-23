52922b9d2a9a3ace0faffa05470776fef9d5a71e
Monzer-gom3a <MonzerMohammed9955@gmail.com>
2024-08-23 16:56:56 +0300

    app initialization

---
+++ lib/app/core/utils/constance.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color.fromARGB(255, 86, 68, 148);
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color greyBack = const Color(0xfff2F2F2);
+  static Color greyText = const Color(0xff7F8C8D);
+}
+
+class CommonStrings {
+  static String urlBase = 'https://hobby-back.onrender.com';
+}
---
+++ lib/app/router/router.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:news_app/app/features/home/view/home.dart';
+
+class AppRoutes {
+  static const HOME = '/home';
+
+  static final routes = [
+    GetPage(name: HOME, page: () => HomePage()),
+  ];
+}
